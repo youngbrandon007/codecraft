96633387039dc69adf7940adc36fbbd98df146fb
robotgenis <youngbrandon007@gmail.com>
2024-10-19 22:42:13 -0400

    fix

--- frontend/dist/assets/handlebars-0FdvIAGm.js
+++ frontend/dist/assets/handlebars-0FdvIAGm.js
@@ -1,4 +1,4 @@
-import{m as i}from"./CodeView-CU2292mN.js";import"./index-BW2JSiMC.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./CodeView-BT-PWPKx.js";import"./index-2jCYg2L5.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.0(f6dc0eb8fce67e57f6036f4769d92c1666cdf546)
  * Released under the MIT license
--- frontend/dist/assets/javascript-B31iyEnx.js
+++ frontend/dist/assets/javascript-B31iyEnx.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-DWfo3dXA.js";import"./CodeView-CU2292mN.js";import"./index-BW2JSiMC.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-C-bnuYS9.js";import"./CodeView-BT-PWPKx.js";import"./index-2jCYg2L5.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.0(f6dc0eb8fce67e57f6036f4769d92c1666cdf546)
  * Released under the MIT license
